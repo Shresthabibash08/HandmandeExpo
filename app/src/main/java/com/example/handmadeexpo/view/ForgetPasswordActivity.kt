3c5dc81447c6e416ed4a0f5dbe0b1172898d8210
Ansh Thapa <anshthapa97@gmail.com>
2025-12-10 12:08:34 +0545

    Started making Forgetpassword activity

---
+++ app/src/main/java/com/example/handmadeexpo/view/ForgetPasswordActivity.kt
@@ -0,0 +1,144 @@
+package com.example.handmadeexpo.view
+
+import android.app.Activity
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Color.Companion.Black
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.handmadeexpo.R
+import com.example.handmadeexpo.ui.theme.Blue
+import com.example.handmadeexpo.ui.theme.White
+
+class ForgetPasswordActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ForgetPasswordBody()
+        }
+    }
+}
+
+@Composable
+fun ForgetPasswordBody() {
+    var email by remember { mutableStateOf("") }
+    val context = LocalContext.current
+    val activity = context as? Activity
+    Scaffold { padding ->
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(padding)
+                .background(White)
+        ) {
+            item {
+                Spacer(modifier = Modifier.height(60.dp))
+                Row(
+                    modifier = Modifier.fillMaxWidth()
+                        .padding(padding),
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.Center
+                ) {
+                    Image(
+                        painter = painterResource(R.drawable.logo),
+                        contentDescription = null,
+                        modifier = Modifier
+                            .size(180.dp)
+                            .clip(CircleShape),
+                        contentScale = ContentScale.Crop
+                    )
+                }
+                Text(
+                    "Forget Password",
+                    modifier = Modifier.fillMaxWidth(),
+                    style = TextStyle(
+                        color = Black,
+                        fontWeight = FontWeight.Bold,
+                        textAlign = TextAlign.Center,
+                        fontSize = 40.sp
+                    )
+                )
+
+                Spacer(modifier = Modifier.height(20.dp))
+
+
+                Text(
+                    "Enter your Email",
+                    modifier = Modifier.fillMaxWidth(),
+                    style = TextStyle(
+                        color = Black,
+                        textAlign = TextAlign.Center,
+                        fontSize = 25.sp
+                    )
+                )
+                Spacer(modifier = Modifier.height(30.dp))
+
+                OutlinedTextField(
+                    value = email,
+                    onValueChange = { data ->
+                        email = data
+                    },
+                    shape = RoundedCornerShape(12.dp),
+                    keyboardOptions = KeyboardOptions(
+                        keyboardType = KeyboardType.Email
+                    ),
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 15.dp),
+                    placeholder = {
+                        Text("Email")
+                    },
+                    colors = TextFieldDefaults.colors(
+                        focusedIndicatorColor = Blue,
+                        unfocusedIndicatorColor = Color.Transparent
+                    )
+                )
+                Spacer(modifier = Modifier.height(20.dp))
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun ForgetPasswordPreview() {
+    ForgetPasswordBody()
+}
