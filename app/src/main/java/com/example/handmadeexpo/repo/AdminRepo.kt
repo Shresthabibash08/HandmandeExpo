62ec6300f1d6e8149da14b1c1ad7a778c106a078
NirajanRana1 <nirajanr115@gmail.com>
2026-01-02 16:08:57 +0545

    code of admin repo

--- app/src/main/java/com/example/handmadeexpo/repo/AdminRepo.kt
+++ app/src/main/java/com/example/handmadeexpo/repo/AdminRepo.kt
@@ -1,4 +1,11 @@
 package com.example.handmadeexpo.repo
 
+
+import com.example.handmadeexpo.model.BuyerModel
+import com.example.handmadeexpo.model.SellerModel
+
 interface AdminRepo {
+    fun getSellers(onResult: (List<SellerModel>) -> Unit)
+    fun getBuyers(onResult: (List<BuyerModel>) -> Unit)
+    fun deleteUser(id: String, role: String, onComplete: (Boolean) -> Unit)
 }
