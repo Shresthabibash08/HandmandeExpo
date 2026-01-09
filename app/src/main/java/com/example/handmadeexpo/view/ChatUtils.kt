c88742f068776e2113700479d28f66e0ec9fd8bc
NirajanRana1 <nirajanr115@gmail.com>
2026-01-09 20:49:37 +0545

    Chatutils deleted

--- app/src/main/java/com/example/handmadeexpo/view/ChatUtils.kt
+++
@@ -1,7 +0,0 @@
-package com.example.handmadeexpo.view
-
-object ChatUtils {
-    fun generateChatId(uid1: String, uid2: String): String {
-        return if (uid1 < uid2) "${uid1}_${uid2}" else "${uid2}_${uid1}"
-    }
-}
