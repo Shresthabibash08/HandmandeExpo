45ab0bdf75d3852c67ebf286581972a177cf5fd9
NirajanRana1 <nirajanr115@gmail.com>
2026-01-29 16:58:27 +0545

    mainactivity completed

--- app/src/main/java/com/example/handmadeexpo/MainActivity.kt
+++ app/src/main/java/com/example/handmadeexpo/MainActivity.kt
@@ -1,31 +1,16 @@
 package com.example.handmadeexpo
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.handmadeexpo.ui.theme.HandmadeExpoTheme
-
-// main
+import com.example.handmadeexpo.view.SplashActivity
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-          MainBody()
-        }
+
+        val intent = Intent(this, SplashActivity::class.java)
+        startActivity(intent)
+        finish()
     }
-}
-
-@Composable
-fun MainBody(){
-
 }
